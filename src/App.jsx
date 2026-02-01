85c49f2245021bb2c914133df34e960df487fa86
prateektiwari31 <prateektiwari828@gmail.com>
2026-02-01 16:41:02 +0530

    INITIAL COMMIT

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import LocationTracker from "./LocationTracker";
+
+function App() {
+  return (
+    <div>
+      <h1>Live Location Finder</h1>
+      <LocationTracker />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/LocationTracker.jsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import "./LocationTracker.css";
+
+function LocationTracker() {
+  const [location, setLocation] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const getLocation = () => {
+    setLoading(true);
+
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        fetch("https://live-location-z1tf.onrender.com/api/location", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify({
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+          })
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            setLocation(data);
+            setLoading(false);
+          });
+      },
+      () => {
+        alert("Location permission denied");
+        setLoading(false);
+      }
+    );
+  };
+
+  return (
+    <div className="container">
+      <h1>Live Location Finder</h1>
+
+      <button className="location-btn" onClick={getLocation} disabled={loading}>
+        {loading ? "Fetching location..." : "📍 Get My Location"}
+      </button>
+
+      {location && (
+        <div className="location-card">
+          <p><b>Place:</b> {location.place}</p>
+          <p><b>City:</b> {location.city}</p>
+          <p><b>State:</b> {location.state}</p>
+          <p><b>Pincode:</b> {location.pincode}</p>
+          <p><b>Country:</b> {location.country}</p>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default LocationTracker;
